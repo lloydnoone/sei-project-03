e62edfada54a9b11cccbee3d78b2b10a52e8ebeb
lloydnoone <lloydnoone92@gmail.com>
2019-11-18 16:31:11 +0000

    Ready for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 const dbURI = process.env.MONGODB_URI || `mongodb://localhost/allotted-${env}`
-const port = 4000
+const port = process.env.PORT || 4000
 const secret = 'very secret'
 
 module.exports = { dbURI, port, secret }
